877cf9bbdc97f1238d63487f91ca6a1b25914fea
German Gomez-Herrero <german@innovativetravel.eu>
2016-03-07 18:36:16 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+import secrets_vault.metadata as metadata
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError, RuntimeError):
+    if os.path.isfile("README.md"):
+        long_description = open("README.md").read()
+    else:
+        long_description = metadata.description
+
+setup(
+    name="humilis-secrets-vault",
+    version=metadata.version,
+    author=metadata.authors_string,
+    author_email=metadata.emails[0],
+    url=metadata.url,
+    license=metadata.license,
+    description=metadata.description,
+    long_description=long_description,
+    install_requires=[
+        "humilis>=0.3.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3"],
+    zip_safe=False,
+    entry_points={
+        "humilis.layers": [
+            "secrets-vault=secrets_vault.__init__:get_layer_path"]}
+)
