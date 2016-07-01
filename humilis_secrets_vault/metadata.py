4574e0445d3be10e78e960b5832e81cfb9013f25
German Gomez-Herrero <german@innovativetravel.eu>
2016-07-01 11:11:42 +0200

    Include package data

--- humilis_secrets_vault/metadata.py
+++ humilis_secrets_vault/metadata.py
@@ -2,7 +2,7 @@
 
 package = "humilis_secrets_vault"
 project = "humilis-secrets-vault"
-version = '0.1.0'
+version = '0.2.0'
 description = "Humilis layer that deploys a secrets vault"
 authors = ["German Gomez-Herrero"]
 authors_string = ', '.join(authors)
